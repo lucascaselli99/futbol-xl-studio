feeda158c89ac019ae546bce69cec1d33d60c4da
lucascaselli99 <141095465+lucascaselli99@users.noreply.github.com>
2026-08-01 11:57:11 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4340,7 +4340,14 @@ if (localStorage.getItem('guestMode') === 'true') {
       return;
     }
 
-    if (el.dataset.field && video && !['stateId', 'seriesId', 'formatId', 'contentTypeId', 'priorityId'].includes(el.dataset.field)) {
+    if (el.dataset.field && video && ![
+          'stateId',
+          'seriesId',
+          'formatId',
+          'contentTypeId',
+          'priorityId',
+          'ownerId'
+        ].includes(el.dataset.field)) {
       video[el.dataset.field] = el.value;
       touchAndSaveDebounced(video);
       return;
